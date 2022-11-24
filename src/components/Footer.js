0bda4a80276977a6a1ad00c0431f34d95ef64091
Phillip Zaya <Phillip.zaya@outlook.com>
2022-11-24 15:25:45 +1100

    5th commit

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+        <h2>Created by Phillip © 2022</h2>
+    </footer>
+  )
+}
+
+export default Footer
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,17 +1,17 @@
 import React from 'react'
-import { Link } from 'gatsby'
 import Layout from '../components/Layout'
-import {page, text} from '../examples/home.module.css'
+import {page, text} from '../examples/about.module.css'
 
-export default function Home() {
+const about = () => {
   return (
     <Layout>
-    <div className={page}>
-      <h1>home page</h1>
-      <h1>Hello world</h1>
-      <h1>Hello people</h1>
+      <div className={page}>
+      <h1>About page</h1>
+      <h1>hello world</h1>
       <p className={text}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-    </div>
+      </div>
     </Layout>
   )
 }
+
+export default about
