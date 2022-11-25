cf684337dda9a771b2aa43d8d58a89738be47175
Phillip Zaya <Phillip.zaya@outlook.com>
2022-11-25 16:40:16 +1100

    6th commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import { FiAlignJustify } from 'react-icons/fi'
 
 const Navbar = () => {
   return (
     <nav>
+      <FiAlignJustify />
         <ul>
             <li>
             <Link to='/'>Home</Link>
@@ -13,6 +15,12 @@ const Navbar = () => {
             </li>
             <li>
             <Link to='/contact'>contact</Link>
+            </li>
+            <li>
+            <Link to='/tags'>tags</Link>
+            </li>
+            <li>
+            <Link to='/recipes'>recipes</Link>
             </li>
         </ul>
     </nav>
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,9 +1,38 @@
 import React from 'react'
+import Layout from '../components/Layout'
 
-export default function contact() {
+const Contact = () => {
   return (
-    <div>
-      <h1>contact page</h1>
-    </div>
+    <Layout>
+      <main className='page'>
+        <section className='contact-page'>
+          <article className='contact-info'>
+          <h3>Want To Get In Touch?</h3>
+          <p>Affogato fingerstache hammock, selvage yuccie lo-fi art party. Schlitz ugh gluten-free flannel. Tbh disrupt selvage, irony locavore JOMO air plant big mood you probably haven't heard of them bitters hella.</p>
+          <p>Kickstarter kinfolk yuccie tattooed squid, franzen copper mug. Offal tote bag chambray vape four dollar toast. PBR&B intelligentsia hexagon poke af, taiyaki biodiesel pork belly master cleanse seitan humblebrag pinterest.</p>
+          <p>Coloring book activated charcoal messenger bag sartorial chicharrones pork belly. Authentic heirloom salvia sartorial meditation kitsch. Banjo umami asymmetrical four dollar toast drinking vinegar vexillologist pop-up kinfolk portland. Prism pour-over gentrify, beard fam put a bird on it pickled wolf air plant.</p>
+          </article>
+          <article>
+            <form className='form contact-form'>
+            <div className='form-row'>
+             <label htmlFor='name'>your name</label>
+             <input type='text' name='name' id='name' />
+            </div>
+            <div className='form-row'>
+             <label htmlFor='email'>your email</label>
+             <input type='text' name='email' id='email' />
+            </div>
+            <div className='form-row'>
+              <label htmlFor='message'>message</label>
+              <textarea name='message' id='message'></textarea>
+            </div>
+            <button type='submit' className='btn block'>submit</button>
+            </form>
+          </article>
+        </section>
+      </main>
+    </Layout>
   )
 }
+
+export default Contact
